78665170baf07bf0788478e9b44ede2bc8851f9b
jjuyeoni <cwa302@naver.com>
2017-11-29 13:22:54 +0000

    get, post 방식

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Create
   get 'posts/new' => 'posts#new'
-  get 'posts/create' => 'posts#create'
+  post 'posts/create' => 'posts#create'
   
   # Read
   root 'posts#index'
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
     # 여기서 :post_id는 url에 담긴 post.id 값을 저장할 변수 
     
   # Delete
-  get 'posts/destroy/:post_id' => 'posts#destroy'
+  post 'posts/destroy/:post_id' => 'posts#destroy'
   
   # Update
   get 'posts/edit/:post_id' => 'posts#edit'
     # 몇번 게시물에 대한 수정인지 알아야하기 때문에 id값 필요
-  get 'posts/update/:post_id' => 'posts#update'
+  post 'posts/update/:post_id' => 'posts#update'
 end
